e18c02e3699bbe20306bb6feb1490a5f85e361fc
Gustavo Leal <32968125+gustavoweb33@users.noreply.github.com>
2018-10-09 14:58:23 -0600

    added files via upload

--- index.js
+++ index.js
@@ -3,16 +3,37 @@ const user = {
     password: ''
 }
 
-
 const usernameInput = document.querySelector('.username');
+const passwordInput = document.querySelector('.password');
+
+
 usernameInput.addEventListener('input',  function(e) {
-    console.log(e);
     user.username = e.target.value;
 });
 
-const passwordInput = document.querySelector('.password');
 passwordInput.addEventListener('input', function(e) {
     user.password = e.target.value;
 });
 
+
+
+
+
+
+
 //font-family: 'Noto Sans', sans-serif;
+
+// function toggle(e) {
+//     if(e.target.style.backgroundColor === '') {
+//         usernameInput.style.backgroundColor = 'rgb(206, 225, 255)';
+//     }
+//     else {
+//         e.target.style.backgroundColor = '';
+//     }
+// }
+
+
+
+// usernameInput.addEventListener('click', function(e) {
+//     toggle(e)
+// });
