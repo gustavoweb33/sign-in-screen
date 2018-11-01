af39fee948b69017bff4aabb64cd15a0cd3be698
gustavoweb33 <lealgustavo33@gmail.com>
2018-11-01 14:32:56 -0600

    updated files

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,23 +1,39 @@
 const userSignUp = document.querySelector('#sign-up-form');
+const emailTitle = document.querySelector('.email');
+const warningParagraph = document.createElement('p');
+warningParagraph.textContent = 'Email already exists';
+warningParagraph.style.color = 'rgb(219, 43, 43)';
+warningParagraph.style.fontSize = '17px';
 
 const getSavedUser = () => {
     const userJSON = localStorage.getItem('user');
-
     return userJSON ? JSON.parse(userJSON) : [];
 }
+
+
 
 let user = getSavedUser();
 
 userSignUp.addEventListener('submit', (e) => {
     e.preventDefault();
+    const firstName = e.target.elements.firstName.value;
+    const lastName = e.target.elements.lastName.value;
+    const email = e.target.elements.email.value;
+    const password = e.target.elements.password.value;
 
-    user.push({
-        firstName: e.target.elements.firstName.value,
-        lastName: e.target.elements.lastName.value,
-        email: e.target.elements.email.value,
-        password: e.target.elements.password.value
-    });
+    if (user.find((object) => object.email === email)) {
+        emailTitle.appendChild(warningParagraph);
+    }
+    else {
+        console.log(user.includes(email));
+        user.push({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password
+        });
+        localStorage.setItem('user', JSON.stringify(user));
+        location.assign('/html/index.html');
+    }
 
-    localStorage.setItem('user', JSON.stringify(user));
-    location.assign('/html/index.html');
 });
